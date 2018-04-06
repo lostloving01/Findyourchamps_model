7a57f0220a5d0fc81fac7fa2916cb5969d52c2f4
lostloving01 <loutsoul10@gmail.com>
2018-04-06 10:01:12 -0400

    create a new class for general win/fail data

--- PlayerRecord.py
+++ PlayerRecord.py
@@ -12,3 +12,33 @@ class PlayerRcd:
         self.res.append(game_res)
         self.champ_use.append(champ)
         self.count += 1
+
+
+class GeneralRcd:
+    def __init__(self):
+        self.game_count = 0
+        self.game_id_set = set([])
+        self.player_count = 0
+        self.player_set = set([])
+        self.champ_use = {}
+        self.champ_result = {}
+
+    def add_a_game(self, game_id):
+        self.game_id_set.add(game_id)
+        self.game_count = self.game_id_set.__len__()
+
+    def add_a_player(self, player):
+        self.player_set.add(player)
+        self.player_count = self.player_set.__len__()
+
+    def add_a_match(self, champ, result):
+        if champ in self.champ_use:
+            self.champ_use[champ] += 1
+        else:
+            self.champ_use[champ] = 1
+            self.champ_result[champ] = 0
+        if result:
+            self.champ_result[champ] += 1
+
+
+
